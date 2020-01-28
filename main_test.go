9c13e2c22359631a00abe46c4e6e83fb9a0d7d4d
wfan <wfan@yahoo-corp.jp>
2020-01-28 17:43:21 +0900

    add test

--- main_test.go
+++ main_test.go
@@ -149,3 +149,22 @@ func Test_run(t *testing.T) {
 		})
 	}
 }
+
+func Test_getVersion(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "default",
+			want: "development version",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getVersion(); got != tt.want {
+				t.Errorf("getVersion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
