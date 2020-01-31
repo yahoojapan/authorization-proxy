3e09fc0c3eaf2acf0aa15152942a08781e86d7a7
wfan <wfan@yahoo-corp.jp>
2020-01-31 13:53:06 +0900

    remove all error handling from glg logging

--- handler/transport.go
+++ handler/transport.go
@@ -17,7 +17,6 @@ limitations under the License.
 package handler
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/yahoojapan/authorization-proxy/config"
@@ -37,9 +36,7 @@ type transport struct {
 func (t *transport) RoundTrip(r *http.Request) (*http.Response, error) {
 	for _, urlPath := range t.cfg.BypassURLPaths {
 		if urlPath == r.URL.Path {
-			if err := glg.Info("Authorization checking skipped on: " + r.URL.Path); err != nil {
-				log.Println("Authorization checking skipped on: " + r.URL.Path)
-			}
+			glg.Info("Authorization checking skipped on: " + r.URL.Path)
 			return t.RoundTripper.RoundTrip(r)
 		}
 	}
