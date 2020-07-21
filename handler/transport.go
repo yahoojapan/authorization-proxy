d85f7b151563df532283dec32c188ffb5d2f64a4
ssuno <ssuno@yahoo-corp.jp>
2020-07-21 13:17:45 +0900

    Set header

--- handler/transport.go
+++ handler/transport.go
@@ -17,7 +17,10 @@ limitations under the License.
 package handler
 
 import (
+	authorizerd "github.com/yahoojapan/athenz-authorizer/v4"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/yahoojapan/authorization-proxy/v3/config"
 	"github.com/yahoojapan/authorization-proxy/v3/service"
@@ -42,10 +45,48 @@ func (t *transport) RoundTrip(r *http.Request) (*http.Response, error) {
 		}
 	}
 
-	if err := t.prov.Verify(r, r.Method, r.URL.Path); err != nil {
+	reqBodyClosed := false
+	if r.Body != nil {
+		defer func() {
+			if !reqBodyClosed {
+				r.Body.Close()
+			}
+		}()
+	}
+
+	p, err := t.prov.Authorize(r, r.Method, r.URL.Path)
+	if err != nil {
 		return nil, errors.Wrap(err, ErrMsgUnverified)
 	}
 
-	r.TLS = nil
-	return t.RoundTripper.RoundTrip(r)
+	req2 := cloneRequest(r) // per RoundTripper contract
+
+	req2.Header.Set("X-Athenz-Principal", p.GetName())
+	req2.Header.Set("X-Athenz-Role", strings.Join(p.GetRoles(), ","))
+	req2.Header.Set("X-Athenz-Domain", p.GetDomain())
+	req2.Header.Set("X-Athenz-Issued-At", strconv.FormatInt(p.GetIssueTime(), 10))
+	req2.Header.Set("X-Athenz-Expires-At", strconv.FormatInt(p.GetExpiryTime(), 10))
+
+	if c, ok := p.(authorizerd.ClientID); ok {
+		req2.Header.Set("X-Athenz-Client-ID", c.GetClientID())
+	}
+
+	req2.TLS = nil
+	// req.Body is assumed to be closed by the base RoundTripper.
+	reqBodyClosed = true
+	return t.RoundTripper.RoundTrip(req2)
+}
+
+// cloneRequest returns a clone of the provided *http.Request.
+// The clone is a shallow copy of the struct and its Header map.
+func cloneRequest(r *http.Request) *http.Request {
+	// shallow copy of the struct
+	r2 := new(http.Request)
+	*r2 = *r
+	// deep copy of the Header
+	r2.Header = make(http.Header, len(r.Header))
+	for k, s := range r.Header {
+		r2.Header[k] = append([]string(nil), s...)
+	}
+	return r2
 }
