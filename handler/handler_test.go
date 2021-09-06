6dfd77f8c0bfba61b93073991c3cbf912a0cc7f8
wfan <wfan@yahoo-corp.jp>
2021-09-06 14:17:48 +0900

    remove fatal test case

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -8,8 +8,6 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/http/httputil"
-	"os"
-	"os/exec"
 	"strconv"
 	"strings"
 	"testing"
@@ -405,78 +403,6 @@ func TestNew(t *testing.T) {
 			got := New(tt.args.cfg, tt.args.bp, tt.args.prov)
 			if err := tt.checkFunc(got); err != nil {
 				t.Errorf("New() error: %v", err)
-			}
-		})
-	}
-}
-
-func TestReverseProxyFatal(t *testing.T) {
-	type args struct {
-		cfg  config.Proxy
-		bp   httputil.BufferPool
-		prov service.Authorizationd
-	}
-	type test struct {
-		name      string
-		args      args
-		checkFunc func(http.Handler) error
-	}
-	tests := []test{
-		func() test {
-			handler := http.HandlerFunc(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				_, err := w.Write([]byte("dummyContent"))
-				if err != nil {
-					w.WriteHeader(http.StatusNotImplemented)
-					return
-				}
-				w.WriteHeader(http.StatusOK)
-			}))
-			srv := httptest.NewServer(handler)
-
-			return test{
-				name: "check fatal, new request failed",
-				args: args{
-					cfg: config.Proxy{
-						Host: "invalid_URL_@@@",
-						Port: func() uint16 {
-							a, _ := strconv.ParseInt(strings.Split(srv.URL, ":")[2], 0, 64)
-							return uint16(a)
-						}(),
-					},
-					bp: infra.NewBuffer(64),
-					prov: &service.AuthorizerdMock{
-						VerifyFunc: func(r *http.Request, act, res string) (authorizerd.Principal, error) {
-							return &PrincipalMock{}, nil
-						},
-					},
-				},
-				checkFunc: func(h http.Handler) error {
-					rw := httptest.NewRecorder()
-					r := httptest.NewRequest("GET", "http://dummy.com", nil)
-					h.ServeHTTP(rw, r)
-					if rw.Code != http.StatusOK {
-						return errors.Errorf("unexpected status code, got: %v, want: %v", rw.Code, http.StatusOK)
-					}
-					return nil
-				},
-			}
-		}(),
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			// test http.NewRequest() fatal in httputil.ReverseProxy.Director with another process (cannot include in test coverage)
-			if os.Getenv("RUN_TEST_REVERSE_PROXY_FATAL") == "1" {
-				got := New(tt.args.cfg, tt.args.bp, tt.args.prov)
-				if err := tt.checkFunc(got); err != nil {
-					t.Errorf("New() error: %v", err)
-				}
-				return
-			}
-			cmd := exec.Command(os.Args[0], "-test.run=TestReverseProxyFatal")
-			cmd.Env = append(os.Environ(), "RUN_TEST_REVERSE_PROXY_FATAL=1")
-			err := cmd.Run()
-			if e, ok := err.(*exec.ExitError); !ok || e.ExitCode() != 1 {
-				t.Errorf("process ran with err: %v, want exit status 1", err)
 			}
 		})
 	}
